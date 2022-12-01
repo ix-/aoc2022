37fae23140d7928604a3f7ef654b0712f11c85fa
ix <morris.weimerskirch@univie.ac.at>
2022-12-01 11:16:48 +0100

    solved day1_1

---
+++ day1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class SearchParty:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = filename
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, filename):
+        self._data =  pd.read_csv(filename, names=["calories"], skip_blank_lines=False)
+        self.data["elf"] = self.data["calories"].isna().cumsum()
+
+    def task_1_1(self):
+        grp = self.data.groupby("elf")
+        self.calories_per_elf = grp["calories"].sum()
+        return self.calories_per_elf.max()
---
+++ main.py
@@ -0,0 +1,6 @@
+from day1 import SearchParty
+
+sp = SearchParty("day1.input")
+
+if __name__ == "__main__":
+    print(sp.task_1_1())
